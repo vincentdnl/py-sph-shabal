e6b4467a42e2d05be49c47b98eb8156807eb7fc8
Vincent Déniel <denielvincent@gmail.com>
2018-04-28 11:11:36 +0200

    Added shabal192, shabal224, shabal384 and shabal512

--- pysphshabalmodule.c
+++ pysphshabalmodule.c
@@ -3,6 +3,36 @@
 #include "sphlib-3.0-c/sph_shabal.h"
 
 
+static PyObject* shabal192(PyObject *self, PyObject *args) {
+    const char *params;
+
+    if (!PyArg_ParseTuple(args, "s", &params))
+        return NULL;
+
+    sph_shabal_context ctx;
+    sph_shabal192_init(&ctx);
+    sph_shabal192(&ctx, (const unsigned char*)params, (int)strlen(params));
+    char ctx_cache[24];
+    sph_shabal192_close(&ctx, ctx_cache);
+
+    return PyBytes_FromStringAndSize(ctx_cache, (Py_ssize_t)24);
+}
+
+static PyObject* shabal224(PyObject *self, PyObject *args) {
+    const char *params;
+
+    if (!PyArg_ParseTuple(args, "s", &params))
+        return NULL;
+
+    sph_shabal_context ctx;
+    sph_shabal224_init(&ctx);
+    sph_shabal224(&ctx, (const unsigned char*)params, (int)strlen(params));
+    char ctx_cache[28];
+    sph_shabal224_close(&ctx, ctx_cache);
+
+    return PyBytes_FromStringAndSize(ctx_cache, (Py_ssize_t)28);
+}
+
 static PyObject* shabal256(PyObject *self, PyObject *args) {
     const char *params;
 
@@ -19,10 +49,54 @@ static PyObject* shabal256(PyObject *self, PyObject *args) {
 }
 
 
+static PyObject* shabal384(PyObject *self, PyObject *args) {
+    const char *params;
+
+    if (!PyArg_ParseTuple(args, "s", &params))
+        return NULL;
+
+    sph_shabal_context ctx;
+    sph_shabal384_init(&ctx);
+    sph_shabal384(&ctx, (const unsigned char*)params, (int)strlen(params));
+    char ctx_cache[48];
+    sph_shabal384_close(&ctx, ctx_cache);
+
+    return PyBytes_FromStringAndSize(ctx_cache, (Py_ssize_t)48);
+}
+
+
+static PyObject* shabal512(PyObject *self, PyObject *args) {
+    const char *params;
+
+    if (!PyArg_ParseTuple(args, "s", &params))
+        return NULL;
+
+    sph_shabal_context ctx;
+    sph_shabal512_init(&ctx);
+    sph_shabal512(&ctx, (const unsigned char*)params, (int)strlen(params));
+    char ctx_cache[64];
+    sph_shabal512_close(&ctx, ctx_cache);
+
+    return PyBytes_FromStringAndSize(ctx_cache, (Py_ssize_t)64);
+}
+
+
 static PyMethodDef py_sph_shabal_methods[] = {
     {
+        "shabal192", shabal192, METH_VARARGS,
+        "Result of shabal hash function on a 192 bits output. Example: shabal192(\"string\").hex()"
+    },{
+        "shabal224", shabal224, METH_VARARGS,
+        "Result of shabal hash function on a 224 bits output. Example: shabal224(\"string\").hex()"
+    },{
         "shabal256", shabal256, METH_VARARGS,
-        "Result of shabal hash function on a 256 bits output. Example: shabal256(\"Hello\").hex()"
+        "Result of shabal hash function on a 256 bits output. Example: shabal256(\"string\").hex()"
+    },{
+        "shabal384", shabal384, METH_VARARGS,
+        "Result of shabal hash function on a 384 bits output. Example: shabal384(\"string\").hex()"
+    },{
+        "shabal512", shabal512, METH_VARARGS,
+        "Result of shabal hash function on a 512 bits output. Example: shabal512(\"string\").hex()"
     },
     {NULL, NULL, 0, NULL}
 };
--- test_shabal.py
+++ test_shabal.py
@@ -1,5 +1,21 @@
-from py_sph_shabal import shabal256
+from py_sph_shabal import shabal192, shabal224, shabal256, shabal384, shabal512
+
+
+def test_shabal192_len_0():
+    assert shabal192("").hex() == "e10dc32232f98b039dbbcfa41269b9cdf67a73c841214c81"
+
+
+def test_shabal224_len_0():
+    assert shabal224("").hex() == "562b4fdbe1706247552927f814b66a3d74b465a090af23e277bf8029"
 
 
 def test_shabal256_len_0():
     assert shabal256("").hex() == "aec750d11feee9f16271922fbaf5a9be142f62019ef8d720f858940070889014"
+
+
+def test_shabal384_len_0():
+    assert shabal384("").hex() == "ff093d67d22b06a674b5f384719150d617e0ff9c8923569a2ab60cda886df63c91a25f33cd71cc22c9eebc5cd6aee52a"
+
+
+def test_shabal512_len_0():
+    assert shabal512("").hex() == "fc2d5dff5d70b7f6b1f8c2fcc8c1f9fe9934e54257eded0cf2b539a2ef0a19ccffa84f8d9fa135e4bd3c09f590f3a927ebd603ac29eb729e6f2a9af031ad8dc6"
