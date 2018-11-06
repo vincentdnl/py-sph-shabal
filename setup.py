734bef7416121aaf07b51c7f896d5322fb0f808b
Vincent Déniel <denielvincent@gmail.com>
2018-11-06 21:34:40 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup, Extension
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 shabal_module = Extension(
     'py_sph_shabal',
     sources=[
@@ -12,5 +15,7 @@ setup(
     name='py_sph_shabal',
     version='0.1.0',
     description='Sphlib 3.0 bindings for Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     ext_modules=[shabal_module],
 )
